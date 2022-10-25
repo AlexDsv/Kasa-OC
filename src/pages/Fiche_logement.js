577b8c0834ab297eb353b8107035db39a6d641ba
AlexDsv <Alex.dsv@hotmail.fr>
2022-10-25 12:00:32 +0200

    Projet terminé

--- src/pages/Fiche_logement.js
+++ src/pages/Fiche_logement.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
 import Accordion from "../components/Accordion";
 import Carousel from "../components/Carousel";
@@ -8,45 +8,54 @@ import Header from "../components/Header";
 import { data } from "../data";
 import LocationInfos from "../components/LocationInfos";
 import StarsRating from "../components/StarsRating";
-import { Navigate } from "react-router-dom";
-import Error_404 from "../pages/Error_404";
 
 const Fiche_logement = () => {
-  return (
-    <div>
-      <Header />
-      <Carousel />
-      <LocationInfos />
-      <StarsRating />
-      <div className="accordions">
-        <Accordion name="Descritpion">
-          {data.map((data, key) => {
-            let locationId = useParams().id;
-            const locationData = [data.description, data.id];
-            if (locationId === locationData[1]) {
-              console.log(useParams().id, locationData[1]);
-              return locationData[0];
-            }
-          })}
-        </Accordion>
-        <Accordion name="Équipements">
-          <ul>
+  const idsArray = new Array();
+
+  let locationId = useParams().id;
+
+  for (let i = 0; i < data.length; i++) {
+    idsArray.push(data[i].id);
+  }
+  console.log(idsArray.includes(locationId));
+  if (!idsArray.includes(locationId)) {
+    return <Navigate to="/404" />;
+  } else {
+    return (
+      <div>
+        <Header />
+        <Carousel />
+        <LocationInfos />
+        <StarsRating />
+        <div className="accordions">
+          <Accordion name="Descritpion">
             {data.map((data, key) => {
               let locationId = useParams().id;
-              const locationEquipments = [data.equipments, data.id];
-              if (locationId === locationEquipments[1]) {
-                return locationEquipments[0].map((equipment) => (
-                  <li key={key++}>{equipment}</li>
-                ));
+              const locationData = [data.description, data.id];
+              if (locationId === locationData[1]) {
+                return locationData[0];
               }
             })}
-          </ul>
-        </Accordion>
-      </div>
+          </Accordion>
+          <Accordion name="Équipements">
+            <ul>
+              {data.map((data, key) => {
+                let locationId = useParams().id;
+                const locationEquipments = [data.equipments, data.id];
+                if (locationId === locationEquipments[1]) {
+                  return locationEquipments[0].map((equipment) => (
+                    <li key={key++}>{equipment}</li>
+                  ));
+                }
+              })}
+            </ul>
+          </Accordion>
+        </div>
 
-      <Footer />
-    </div>
-  );
+        <Footer />
+      </div>
+    );
+  }
 };
 
 export default Fiche_logement;
